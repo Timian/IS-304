353df211647229ded105304e07bbb50b040159ee
Timian <jnuolsen@gmail.com>
2014-03-30 14:57:07 +0200

    Oppdatert
    Oppdatert med bedre css, har også oppdatert
    lagt til et par bilder etc.

--- wp-content/themes/nff/header.php
+++ wp-content/themes/nff/header.php
@@ -8,7 +8,14 @@
         <?php wp_head()?>
     </head>
     <header>
-        <h1><a href=""<?php home_url('/')?>"><?php bloginfo('name')?></a></h1>
+        <h1><a href=""<?php home_url('/')?>><?php bloginfo('name')?></a></h1>
+        <!-- denne må kobles opp mot server/database som er aktuell-->
+        <div id='login' class="login"> Logg inn<br>
+                <form>
+                    Brukernavn: <input type="text" name="Brukernavn"><br> 
+                    Passord: <input type="text" name="passord">
+                    <input type="submit" name="LOGG INN">
+                </form>
     </header>
     
     <nav>
